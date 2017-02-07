fbdf78a7672b43640727edec9e5bd051660dca41
Natali Donova <romanevg001@gmail.com>
2017-02-07 12:49:06 +0300

    search

--- src/app/search/search-form/search-form.component.ts
+++ src/app/search/search-form/search-form.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.scss']
+  styleUrls: ['./search-form.component.scss'],
+  outputs: ['searchFormChange']
 })
 export class SearchFormComponent implements OnInit {
   searchForm: FormGroup;
+  searchFormChange = new EventEmitter<FormGroup>();  
   from: FormControl;
+  to: FormControl;
+  instock: FormControl;
+  priceFrom: FormControl;
+  priceTo: FormControl;
+  color: FormControl;
+  colorList: ['Red', 'White', 'Black', 'Blue', 'Yellow', 'Green'];
 
   constructor(private fb: FormBuilder) { 
     this.from = fb.control('');
-    
+    this.to = fb.control('');
+    this.instock = fb.control('');
+    this.priceFrom = fb.control('');
+    this.priceTo = fb.control('');
+    this.color = fb.control('');
+
     this.searchForm = fb.group({
-      from: this.from
+      from: this.from,
+      to: this.to,
+      instock: this.instock,
+      priceFrom: this.priceFrom,
+      priceTo: this.priceTo,
+      color: this.color
     });
+
+    this.searchForm.valueChanges
+    .subscribe((newVal)=>{
+      this.searchFormChange.emit(newVal);
+    })
   }
 
   ngOnInit() {
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,4 +12,8 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
   }
 
+  makeFilter($event){
+      console.log($event)
+  }
+
 }
